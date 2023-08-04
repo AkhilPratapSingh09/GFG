50fc77b68f3c786ef852338fe2c65100d5dcaa65
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-08-04 20:59:24 +0530

    Create ReverseStack.java

---
+++ ReverseStack.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class ReverseStack {
+    class Solution
+{ 
+    static void reverse(Stack<Integer> s)
+    {
+        // add your code here
+          // add your code here
+        // s.reverse();
+        ArrayList<Integer>ar=new ArrayList<>();
+        for(int i=s.size()-1 ;i>=0;i--){
+            ar.add(s.pop());
+        }
+        for(int i=0 ;i<ar.size();i++){
+            s.push(ar.get(i));
+        }
+        
+        
+    }
+    
+}
+}

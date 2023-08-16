16feeece3b2de7495d6e388ddca87630b838ea56
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-08-16 08:54:31 +0530

    Create NthCatlanNo.java

---
+++ NthCatlanNo.java
@@ -0,0 +1,21 @@
+public class NthCatlanNo {
+    class Solution {
+        public static int findCatalan(int n) {
+            // code here
+            if(n<=1)
+            return 1;
+            int mod=(int)1e9+7;
+            int[] dp=new int[n+1];
+            dp[0] = 1;
+            dp[1] = 1;
+            for(int i=2;i<=n;i++){
+               // dp[i]=0;
+                for(int j=0;j<i;j++){
+                    dp[i] = (int)(dp[i]%mod + (long)dp[j]%mod * dp[i-j-1]%mod)%mod;
+                }
+            }
+            return dp[n];
+        }
+    }
+          
+}

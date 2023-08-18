c32cba69f291d587395cc74a3bffafeb2f681fe1
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-08-18 15:01:20 +0530

    Create LeadersInArray.java

---
+++ LeadersInArray.java
@@ -0,0 +1,20 @@
+public class LeadersInArray {
+    class Solution{
+        static ArrayList<Integer> leaders(int arr[], int n){
+            ArrayList<Integer> a=new ArrayList<>();
+            int i=n-2;
+            int max=arr[n-1];
+           a.add(max);
+            while(i>=0){
+                if(arr[i]>=max){
+                max=arr[i];
+                a.add(max);
+                }
+                i--;
+            }
+           Collections.reverse(a);
+            return a;
+        }
+    }
+    
+}

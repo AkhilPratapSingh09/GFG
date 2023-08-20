f48fc59f683fb5f682936c714cc5704543a53d6d
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-08-20 16:26:27 +0530

    Create NoOfOccurence.java

---
+++ NoOfOccurence.java
@@ -0,0 +1,15 @@
+public class NoOfOccurence {
+    
+class Solution {
+    int count(int[] arr, int n, int x) {
+        // code here
+        int count=0;
+        for(int i=0;i<arr.length; i++){
+            if(arr[i]==x){
+                count++;
+            }
+        }
+        return count;
+    }
+}
+}

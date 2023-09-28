aa5431314b0763ccedf2f7b2bc7209798dc09a5e
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-09-28 13:15:07 +0530

    Create WaveArr.java

---
+++ WaveArr.java
@@ -0,0 +1,12 @@
+public class WaveArr {
+    class Solution {
+        public static void convertToWave(int n, int[] a) {
+            for (int i = 0; i < n - 1; i += 2) {
+                int temp = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = temp;
+            }
+        }
+    }
+    
+}

30aa926d6ec77a8387884c6f42636a19ea4ef339
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-10-09 14:12:56 +0530

    Create HeightBinaryTree.java

---
+++ HeightBinaryTree.java
@@ -0,0 +1,17 @@
+import org.w3c.dom.Node;
+
+public class HeightBinaryTree {
+    class Solution {
+    //Function to find the height of a binary tree.
+    int height(Node node) 
+    {
+        // code here 
+        if(node==null){
+            return 0 ;
+        }
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+        return Math.max(leftHeight,rightHeight)+1;
+    }
+}
+}

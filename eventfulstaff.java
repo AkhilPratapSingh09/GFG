3ca2cf74d296f5900d848aba09645e52ebb23fa6
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-10-18 21:58:06 +0530

    Create eventfulstaff.java

---
+++ eventfulstaff.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class eventfulstaff {
+    class Solution {
+
+    List<Integer> eventualSafeNodes(int V, List<List<Integer>> adj) {
+
+        List<Integer> res=new ArrayList<>();
+      
+        boolean[] visit=new boolean[V];
+        for(int i=0;i<V;i++){
+            if(!isCycle(i,adj,visit)){
+                res.add(i);
+            }
+        }
+        return res;
+    }
+    private static boolean isCycle(int source,List<List<Integer>> adj,boolean[] visit){
+       
+        visit[source]=true;
+        
+        for(Integer it:adj.get(source)){
+            if(!visit[it]){
+                if(isCycle(it,adj,visit)) return true;
+            }else return true;
+    
+        }
+        visit[source]=false;
+        return false;
+    }
+}
+}

1d8c19f98cf5d8baa720aa4b5d43a5b3e4ec43a6
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-10-21 13:55:32 +0530

    Create sumofalldiv.java

---
+++ sumofalldiv.java
@@ -0,0 +1,12 @@
+public class sumofalldiv {
+    class Solution{
+        static long sumOfDivisors(int N){
+            // code here
+            long ans = 0;
+            for(int i =1;i<=N;i++){
+                ans += (N/i)*i; 
+            }
+            return ans;
+        }
+    }
+}

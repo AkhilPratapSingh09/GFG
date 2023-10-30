c4db05268ec939f7f7a716ff59b891d440cc02b6
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-10-30 10:01:07 +0530

    Create sumofallxorpairs.java

---
+++ sumofallxorpairs.java
@@ -0,0 +1,22 @@
+public class sumofallxorpairs {
+    class Solution{
+   
+        // Function for finding maximum and value pair
+        public long sumXOR (int arr[], int n) {
+            long ans=0;
+            for(int i=0;i<32;i++)
+            {
+                int ones=0;
+                for(int x:arr)
+                {
+                    if((x&(1<<i))!=0)  
+                        ones++;
+                }
+                ans+=(((long)ones*(n-ones))*(1<<i));
+            }
+            return ans;
+        }
+        
+        
+    }
+}

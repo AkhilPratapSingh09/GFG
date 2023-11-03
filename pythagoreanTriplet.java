c4c38a3923dca5fdb616c0be62c197042010c4cb
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-11-03 15:38:26 +0530

    Create pythagoreanTriplet.java

---
+++ pythagoreanTriplet.java
@@ -0,0 +1,36 @@
+public class pythagoreanTriplet {
+    class Solution {
+        boolean checkTriplet(int[] arr, int n) {
+            
+            int max = Integer.MIN_VALUE;
+            for(int i=0;i<n;i++){
+                arr[i] = arr[i] * arr[i];
+            }
+            //find max value;
+            for(int i=0;i<n;i++){
+                max = Math.max(max,arr[i]);
+            }
+            Set<Integer> set = new HashSet<>();
+            for(int i=0;i<n;i++){
+                set.add(arr[i]);
+            }
+            
+    
+            for(int i=max;i>0;i--){
+                if(set.contains(i)){
+                    for(int j=0;j<n;j++){
+                        if(arr[j] == max){
+                            continue;
+                        }
+                        if(set.contains(Math.abs(i-arr[j]))){
+                            return true;
+                        }
+                        
+                    }
+                }
+            }
+            return false;
+            
+        }
+    }
+}

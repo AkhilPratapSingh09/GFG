b1c30c0a9dad2765590d2d87189570218a53ec86
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-11-28 15:23:09 +0530

    Create sumofependencies.java

---
+++ sumofependencies.java
@@ -0,0 +1,15 @@
+public class sumofependencies{
+    class Solution {
+        int sumOfDependencies(ArrayList<ArrayList<Integer>> adj, int V) {
+            int sum=0;
+            
+            for(int i=0;i<V;i++){
+                for(Integer it:adj.get(i)){
+                    sum++;
+                }
+            }
+            return sum;
+        }
+    };
+    
+}

cc6717e163fa4f96874cfffacd007625331fea8e
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-11-30 14:57:06 +0530

    Create shpa.java

---
+++ shpa.java
@@ -0,0 +1,17 @@
+public class shpa{
+    class Solution{
+        public int minStep(int n)
+        {
+            // code here
+            int c=0;
+            while(n!=1){
+                if(n%3==0) {n=n/3;}
+                else{
+                n--;}
+                c++;
+                
+            } return c;
+           
+        }
+    }
+}

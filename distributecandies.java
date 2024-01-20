81b491754bed45e2d6484c2f920502c5100cd0f6
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2024-01-20 20:19:39 +0530

    Create distributecandies.java

---
+++ distributecandies.java
@@ -0,0 +1,35 @@
+import org.w3c.dom.Node;
+
+public class distributecandies {
+    class Solution
+{
+       int rahee = 0;
+    public  int distributeCandy(Node root)
+    {
+        //code here
+     
+        gg(root);
+        return rahee;
+    }
+    public  int gg(Node root)
+    {
+        if(root == null) 
+        {
+            return 0;
+        }
+        
+        int left = gg(root.left);
+        int right = gg(root.right);
+        rahee += Math.abs(left) + Math.abs(right);
+        return root.data + left + right - 1;
+    }
+}
+
+..... see less
+
+0
+
+2
+
+    
+}

5f4a738ceefb8b0df0d5ff233351c58da7eb778f
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2024-02-19 17:29:38 +0530

    Create gamestring.java

---
+++ gamestring.java
@@ -0,0 +1,48 @@
+import java.util.HashMap;
+
+public class gamestring {
+    class Solution{
+
+//this method returns char with the highest value
+     static char hv( HashMap<Character,Integer> h)
+     {
+         int m=-1;
+         char cch='z';
+      for(Character i:h.keySet())
+      {
+          m=Math.max(m,h.get(i));
+          if(m==h.get(i))
+          {
+              cch=i;
+          }
+      } 
+      return cch;
+     }
+    
+    static int minValue(String s, int k){
+       HashMap<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<s.length();i++)
+        { char ch=s.charAt(i);
+            if(map.containsKey(ch))
+            {
+               map.put(ch,map.get(ch)+1);
+            }
+            else
+            {
+                map.put(ch,1);
+            }
+        }
+        int ans=0;
+        while(k>0)
+        {
+           map.put(hv(map),map.get(hv(map))-1) ;
+           k--;
+        }
+        for(Character i:map.keySet())
+        {
+               ans+=map.get(i)*map.get(i);  
+        }
+        return ans;
+    }
+}
+}

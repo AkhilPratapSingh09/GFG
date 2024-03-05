379d839852a75d8da8a77c514410b4db762e03b0
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2024-03-05 11:06:38 +0530

    Create maxindex.java

---
+++ maxindex.java
@@ -0,0 +1,19 @@
+public class maxindex {
+    class Solution {
+        public int minimumLength(String s) {
+             int i = 0, j = s.length() - 1;
+             while (i < j && s.charAt(i) == s.charAt(j)) {
+                 char c = s.charAt(j);
+                 while (i <= j && s.charAt(i) == c) {
+                     ++i;
+                 }
+                 while (i <= j && s.charAt(j) == c) {
+                     --j;
+                 }
+             }
+             return j - i + 1;
+         }
+     }
+     
+     
+}

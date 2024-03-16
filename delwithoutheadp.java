e4e4f1c72c9e680ae7b92c03cb27205c0fbb4079
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2024-03-16 17:18:26 +0530

    Create delwithoutheadp.java

---
+++ delwithoutheadp.java
@@ -0,0 +1,23 @@
+public class delwithoutheadp {
+    class Solution {
+        void deleteNode(Node dn) {
+            if (dn == null || dn.next == null) {
+                return;
+            }
+            while (dn.next.next != null) {
+                dn.data = dn.next.data;
+                dn = dn.next;
+            }
+            int lastNodeValue = dn.next.data;
+            dn.next = null;
+            dn.data = lastNodeValue;
+        }
+    }
+    
+    
+    ..... see less
+    
+    0
+    
+    
+}

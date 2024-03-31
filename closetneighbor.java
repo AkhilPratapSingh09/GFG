b0c98d6cc15816fb025ac4e42ce83f3287736164
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2024-03-31 08:38:39 +0530

    Create closetneighbor.java

---
+++ closetneighbor.java
@@ -0,0 +1,19 @@
+import org.w3c.dom.Node;
+
+public class closetneighbor {
+    class Solution {
+    public static int findMaxForN(Node root, int n) {
+        Node curr = root;
+        int res = -1;
+        while(curr != null){
+            if(curr.key <= n){
+                res = curr.key;
+                curr = curr.right;
+            }else{
+                curr = curr.left;
+            }
+        }
+        return res;
+    }
+}
+}

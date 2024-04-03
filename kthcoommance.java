b819ca9851caa28bd924b3d490123e36c293d8d3
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2024-04-03 21:16:34 +0530

    Create kthcoommance.java

---
+++ kthcoommance.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.Node;
+
+public class kthcoommance {
+    class Solution {
+    public int kthCommonAncestor(Node root, int k, int x, int y) {
+        if (x == y) return -1;
+        List<Integer> list = new ArrayList<>();
+        while (root != null){
+            list.add(root.data);
+            if (x < root.data && y < root.data) root = root.left;
+            else if (x > root.data && y > root.data) root = root.right;
+            else break;
+        }
+        if (k > list.size()) return -1;
+        return list.get(list.size() - k);
+    }
+}
+}
